68b00e9470c6af5192023918c77368c4c192883e
Elixer Prince <dleith@stu.themico.edu.jm>
2024-09-19 17:00:43 -0500

    Add vector program

---
+++ week-1-2/vector-intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int myArray1[] = {1, 2, 3, 4, 5};
+  std::string myArray2[5] = {"one", "two", "three", "four", "five"};
+  std::vector <int> myVector; //?Resizable array
+
+  std::cout << myArray1[1] << std::endl;
+  std::cout << myArray2[3];
+
+  return 0;
+} 
