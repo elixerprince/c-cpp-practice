90ed72a7adc8b232e717f762e0a863c037a6c745
Elixer Prince <dleith@stu.themico.edu.jm>
2024-09-19 17:31:16 -0500

    Update variables.exe

--- week-1-2/variables.cpp
+++ week-1-2/variables.cpp
@@ -1,22 +1,41 @@
-#include <iostream> //Include the Standard Input/Output library
+#include <iostream>
+#include <vector>
+#include <string>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
+struct Person {
+    std::string name;
+    int age;
+    float height;
+  };
 
-/* GLOBAL VARIABLES */
-string fName;
-int age;
-
-/* MAIN FUNCTION (The function that gets executed) */
 int main() {
-  cout << "What is your first name?" << endl;
-  cin >> fName; //Apparently the cin automatically adds a line break after itself
-  cout << "\nWhat is your age?" << endl;
-  cin >> age;
+  int myInt = 1;
+  float myFloat = 3.14;
+  double myDouble = 3.14159;
+  char myChar = 'A';
+  bool myBool = true;
+  std::string myString = "Daniel Leith";
+  char myArray[] = {'A', 'B', 'C', 'D', 'E'};
+  std::vector <std::string> myVector = {"one", "two", "three"};
 
-  cout << "\nHello " << fName << ", you are " << age << " years old!" << endl;
+  std::cout << "This is my integer: " << myInt << std::endl;
+  std::cout << "This is my float: " << myFloat << std::endl;
+  std::cout << "This is my double: " << myDouble << std::endl;
+  std::cout << "This is my character: " << myChar << std::endl;
+  std::cout << "This is my boolean: " << myBool << std::endl;
+  std::cout << "This is my string: " << myString << std::endl;
+  std::cout << "This is the third entry in my array: " << myArray[2] << std::endl;
+  std::cout << "This is the second entry in my vector: " << myVector[1] << std::endl;
+
+  Person micoStudent1;
+
+  micoStudent1.name = "Daniel Leith";
+  micoStudent1.age = 21;
+  micoStudent1.height = 5.75;
+
+  std::cout << "\nThis is the name of the student: " << micoStudent1.name << std::endl;
+  std::cout << "This is the age of the student: " << micoStudent1.age << std::endl;
+  std::cout << "This is the height of the student: " << micoStudent1.height;
 
   return 0;
 }
